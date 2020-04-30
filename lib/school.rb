3b5ca0ff1073ded042d51fb5ecec3aa8c6811cf8
Dwight Nodolf <dnodolf@gmail.com>
2020-04-30 18:01:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,39 +21,10 @@ class School
      @roster.values_at(grade).flatten
   end
   
-  class School
-  
-  attr_reader :name, :roster
-  attr_accessor :grade
-  
-  def initialize(name, roster = {})
-    @name = name
-    @roster = roster
-  end
-  
-  def add_student(name, grade)
-    if roster.key?(grade) == true
-      @roster[grade] << name
-    else 
-      roster[grade] = []
-      roster[grade] << name
-    end
-  end
-  
-  def grade(grade)
-     @roster.values_at(grade).flatten
-  end
-  
+  #hash.sort_by(&:first).map { |k, v| [k, v.sort]}.to_h
   def sort
+    @roster.sort_by(&:first).map { |k, v| [k, v.sort]}.to_h
     
-  end
-end
-
-  
-end
-
-  def sort
-    @roster.sort_by(& :last)           
     
     
   end
